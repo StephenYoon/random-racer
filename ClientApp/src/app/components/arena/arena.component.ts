960caed786c4c35178b11edd9062a42b4f47ffa5
Stephen Yoon <yoonic@gmail.com>
2019-09-07 14:54:49 -0400

    Converting less to scss

--- ClientApp/src/app/components/arena/arena.component.ts
+++ ClientApp/src/app/components/arena/arena.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Dice } from '../../models/dice';
+import { DiceSet } from '../../models/diceSet';
+import { DiceSetCollection } from '../../data/diceSetCollection'
 
 @Component({
   selector: 'app-arena',
   templateUrl: './arena.component.html',
-  styleUrls: ['./arena.component.less']
+  styleUrls: ['./arena.component.scss']
 })
 export class ArenaComponent implements OnInit {
 
+  private dice1: Dice;
+  private dice2: Dice;
+  private dice3: Dice;
+  
   constructor() { }
 
   ngOnInit() {
