d6645ed1f26efe5886921fa7fe24c0a855de20e4
Stephen Yoon <yoonic@gmail.com>
2019-09-09 19:49:36 -0400

    Added button to roll dice

--- ClientApp/src/app/components/arena/arena.component.ts
+++ ClientApp/src/app/components/arena/arena.component.ts
@@ -11,6 +11,7 @@ import { DiceSetCollection } from '../../data/diceSetCollection'
 export class ArenaComponent implements OnInit {
 
   public playerDiceSet: Dice[];
+  public diceSetKey: string = "regular";
 
   private diceSetCollection: DiceSetCollection = new DiceSetCollection;
   
@@ -18,9 +19,27 @@ export class ArenaComponent implements OnInit {
 
   ngOnInit() {
     this.playerDiceSet = [
-      this.diceSetCollection['regular'].dices[0],      
-      this.diceSetCollection['regular'].dices[1],
-      this.diceSetCollection['regular'].dices[2]
+      this.diceSetCollection[this.diceSetKey].dices[0],      
+      this.diceSetCollection[this.diceSetKey].dices[1],
+      this.diceSetCollection[this.diceSetKey].dices[2]
     ];    
+  }
+
+  getPlayerDiceSet(): Dice[] {
+    return this.playerDiceSet;
+  }
+
+  // Get a new set of dice
+  // TODO: opportunities to refactor this below, but for now it's "okay"
+  rollDice(dices: Dice[]) {
+    for (let i = 0; i < this.playerDiceSet.length; i++) {
+      let randomIndex = this.randomIntFromInterval(1, 6);
+      this.playerDiceSet[i] = this.diceSetCollection[this.diceSetKey].dices[randomIndex - 1]
+    }
+  }
+
+  // Random number generator, min and max included.
+  randomIntFromInterval(min, max): number {
+    return Math.floor(Math.random() * (max - min + 1) + min);
   }
 }
