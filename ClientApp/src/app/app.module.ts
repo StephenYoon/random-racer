3345773e1f4c79a827d33b712c010f58aca426ed
Stephen Yoon <yoonic@gmail.com>
2019-09-21 17:46:53 -0400

    Initial Firebase integration

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArenaComponent } from './components/arena/arena.component';
 import { DiceComponent } from './components/dice/dice.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArenaComponent,
-    DiceComponent
+    DiceComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
